a6d3e8daf73b18a944fcf773c5242b99e19f09bf
splionar <31429575+splionar@users.noreply.github.com>
2017-08-29 14:47:36 +0800

    Add SD Card Feature

--- DS18B20_2.ino
+++ DS18B20_2.ino
@@ -1,10 +1,18 @@
 /*-----( Import needed libraries )-----*/
+#include <SPI.h> //Load SPI library
+#include <SD.h> //Load SD Library
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
 /*-----( Declare Constants )-----*/
 #define ONE_WIRE_BUS_IN 2 /*-(Connect to Pin 2 )-*/
 #define ONE_WIRE_BUS_OUT 4 /*-(Connect to Pin 4 )-*/
+
+// for the data logging shield, define pin number for the SD cs line
+const int chipSelect = 10; //Select pin 10
+
+// Inizialitizing logging file
+File SensorData;
 
 /*-----( Declare objects )-----*/
 /* Set up a oneWire instance to communicate with any OneWire device*/
@@ -16,7 +24,8 @@ DallasTemperature sensorsIn(&ourWireIn);
 DallasTemperature sensorsOut(&ourWireOut);
 
 /*-----( Declare Variables )-----*/
-
+float sensor_in;
+float sensor_out;
 
 void setup() /*----( SETUP: RUNS ONCE )----*/
 {
@@ -30,8 +39,19 @@ void setup() /*----( SETUP: RUNS ONCE )----*/
   /*-( Start up the DallasTemperature library )-*/
   sensorsIn.begin();
   sensorsOut.begin();
-}/*--(end setup )---*/
+  
+  //setup SD card
+   Serial.print("Initializing SD card...");
 
+  // see if the SD card is present and can be initialized:
+  if (!SD.begin(chipSelect)) {
+    Serial.println("Card failed, or not present");
+    // don't do anything more:
+    return;
+  } //end if loop
+  Serial.println("card initialized.");
+  
+}/*--(end setup )---*/
 
 void loop() /*----( LOOP: RUNS CONSTANTLY )----*/
 {
@@ -41,15 +61,31 @@ void loop() /*----( LOOP: RUNS CONSTANTLY )----*/
   sensorsOut.requestTemperatures(); // Send the command to get temperatures
   Serial.println("DONE");
   
+  //Set variable values
+  sensor_in = sensorsIn.getTempCByIndex(0);
+  sensor_out = sensorsOut.getTempCByIndex(1);
+  
+  //Open file to log data in.
+  SensorData = SD.open("datalog.txt", FILE_WRITE);
+  
+  if (SensorData){ //If SensorData = TRUE, print to serial monitor and log data to SensorData
+  //Print to serial monitor
   Serial.print("Device In = ");
-  Serial.print(sensorsIn.getTempCByIndex(0));
+  Serial.print(sensor_in);
   Serial.println(" Degrees C");
 
   Serial.print("Device Out = ");
-  Serial.print(sensorsOut.getTempCByIndex(1));
+  Serial.print(sensor_out);
   Serial.println(" Degrees C");
 
   delay(5000);
+  
+  //Log to SD Card
+  SensorData.print(sensor_in);
+  SensorData.print(",");
+  SensorData.println(sensor_out);
+  }   //end if loop
+  
 }/* --(end main loop )-- */
 
 /* ( THE END ) */
