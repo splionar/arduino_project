e0b851b4a7178e7ea451f54dc191f6e1d6154bd0
splionar <31429575+splionar@users.noreply.github.com>
2017-08-30 14:18:53 +0800

    Added RTC and SD Card Feature

--- DS18B20_2.ino
+++ DS18B20_2.ino
@@ -1,8 +1,10 @@
 /*-----( Import needed libraries )-----*/
 #include <SPI.h> //Load SPI library
 #include <SD.h> //Load SD Library
-#include <OneWire.h>
-#include <DallasTemperature.h>
+#include <OneWire.h> //Load library for temperature sensing
+#include <DallasTemperature.h> //Load library for temperature sensing
+#include <RTClib.h> //Load library for RTC
+#include <Wire.h> //Load library for RTC
 
 /*-----( Declare Constants )-----*/
 #define ONE_WIRE_BUS_IN 2 /*-(Connect to Pin 2 )-*/
@@ -11,8 +13,10 @@
 // for the data logging shield, define pin number for the SD cs line
 const int chipSelect = 10; //Select pin 10
 
+RTC_PCF8523 rtc; //Calling PCF8523 RTC
+
 // Inizialitizing logging file
-File SensorData;
+//File SensorData;
 
 /*-----( Declare objects )-----*/
 /* Set up a oneWire instance to communicate with any OneWire device*/
@@ -26,22 +30,25 @@ DallasTemperature sensorsOut(&ourWireOut);
 /*-----( Declare Variables )-----*/
 float sensor_in;
 float sensor_out;
+char timestamp[20];
 
 void setup() /*----( SETUP: RUNS ONCE )----*/
 {
   /*-(start serial port to see results )-*/
-  delay(1000);
   Serial.begin(9600);
-  Serial.println("YourDuino.com: Temperature Sensor Test Program");
+  rtc.begin();
+  DateTime now = rtc.now();
+  sprintf(timestamp, "%02d/%02d/%02d %02d:%02d:%02d", now.day(), now.month(), now.year(),now.hour(), now.minute(), now.second());  
   Serial.println("Temperature Sensor: DS18B20");
-  delay(1000);
+  Serial.print("Start time: ");
+  Serial.println(timestamp);
 
   /*-( Start up the DallasTemperature library )-*/
   sensorsIn.begin();
-  sensorsOut.begin();
+  sensorsOut.begin(); 
   
   //setup SD card
-   Serial.print("Initializing SD card...");
+  Serial.print("Initializing SD card...");
 
   // see if the SD card is present and can be initialized:
   if (!SD.begin(chipSelect)) {
@@ -50,6 +57,22 @@ void setup() /*----( SETUP: RUNS ONCE )----*/
     return;
   } //end if loop
   Serial.println("card initialized.");
+
+  File SensorData = SD.open("test.txt", FILE_WRITE);
+  
+  if (SensorData){ //If SensorData = TRUE, print to serial monitor and log data to SensorData
+    //Log to SD Card
+    SensorData.print("Time stamp");
+    SensorData.print(",");
+    SensorData.print("Temperature IN");
+    SensorData.print(",");
+    SensorData.println("Temperature OUT");
+    SensorData.close();
+  }   //end if
+  else
+  {
+    Serial.println("There is an error in initializing .txt file");
+  }
   
 }/*--(end setup )---*/
 
@@ -60,31 +83,44 @@ void loop() /*----( LOOP: RUNS CONSTANTLY )----*/
   sensorsIn.requestTemperatures(); // Send the command to get temperatures
   sensorsOut.requestTemperatures(); // Send the command to get temperatures
   Serial.println("DONE");
-  
+
+  DateTime now = rtc.now();
+  sprintf(timestamp, "%02d/%02d/%02d %02d:%02d:%02d", now.day(), now.month(), now.year(),now.hour(), now.minute(), now.second());  
   //Set variable values
   sensor_in = sensorsIn.getTempCByIndex(0);
   sensor_out = sensorsOut.getTempCByIndex(1);
   
   //Open file to log data in.
-  SensorData = SD.open("datalog.txt", FILE_WRITE);
+  File SensorData = SD.open("test.txt", FILE_WRITE);
   
   if (SensorData){ //If SensorData = TRUE, print to serial monitor and log data to SensorData
-  //Print to serial monitor
-  Serial.print("Device In = ");
-  Serial.print(sensor_in);
-  Serial.println(" Degrees C");
+    //Print to serial monitor
+    Serial.print("Date/Time : ");
+    Serial.println(timestamp);
+    Serial.print("Device In = ");
+    Serial.print(sensor_in);
+    Serial.println(" Degrees C");
 
-  Serial.print("Device Out = ");
-  Serial.print(sensor_out);
-  Serial.println(" Degrees C");
-
-  delay(5000);
+    Serial.print("Device Out = ");
+    Serial.print(sensor_out);
+    Serial.println(" Degrees C");
   
-  //Log to SD Card
-  SensorData.print(sensor_in);
-  SensorData.print(",");
-  SensorData.println(sensor_out);
+    delay(1000);
+  
+    //Log to SD Card
+    Serial.print("Logging to SD Card...");
+    SensorData.print(timestamp);
+    SensorData.print(",");
+    SensorData.print(sensor_in);
+    SensorData.print(",");
+    SensorData.println(sensor_out);
+    SensorData.close();
+    Serial.println("DONE");
   }   //end if loop
+  else
+  {
+    Serial.println("There is an error in initializing .txt file");
+  }
   
 }/* --(end main loop )-- */
 
