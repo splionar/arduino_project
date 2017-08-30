618bc4e8954de0a084600a8767718ddfe6db57ce
splionar <31429575+splionar@users.noreply.github.com>
2017-08-30 14:20:12 +0800

    Add files via upload
    Synchronize time between Arduino and PC

---
+++ init_set_RTC_PCF8523.ino
@@ -0,0 +1,28 @@
+#include <RTClib.h>
+#include <Wire.h>
+
+RTC_PCF8523 rtc;
+
+char buf1[20];
+
+void setup() 
+{
+  Serial.begin(9600);
+  Wire.begin();
+
+  rtc.begin();
+  rtc.adjust(DateTime(F(__DATE__),F(__TIME__)));
+
+}
+
+void loop()
+{
+  DateTime now = rtc.now();
+
+  sprintf(buf1, "%02d/%02d/%02d %02d:%02d:%02d", now.day(), now.month(), now.year(),now.hour(), now.minute(), now.second());  
+  
+  Serial.print(F("Date/Time: "));
+  Serial.println(buf1);
+
+  delay(1000);
+}
