3c352b113ec8c0c1f92073d3e3a7ab239c38be1c
Stefan Putra Lionar <31429575+splionar@users.noreply.github.com>
2017-09-12 11:47:37 +0800

    Update temperature_plot.py

--- temperature_plot.py
+++ temperature_plot.py
@@ -2,10 +2,10 @@ import csv
 import pandas as pd
 from matplotlib import pyplot as plt
 import matplotlib.dates as mdates
-#import matplotlib.ticker as plticker
+import matplotlib.ticker as plticker
 
 ##Retrieving Data
-filename = 'test_050917.csv'
+filename = 'outdoor_shaded.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
@@ -21,17 +21,28 @@ with open(filename) as f:
         temperature_out.append(float(row[2]))
         temperature_ambient.append(float(row[3]))
 
+#Removing noise
+for i in range(len(temperature_in)):
+    if temperature_in[i] == -127.00:
+        temperature_in[i] = temperature_in[i-1]
+
+    if temperature_out[i] == -127.00:
+        temperature_out[i] = temperature_out[i-1]
+
+    if temperature_ambient[i] == -127.00:
+        temperature_ambient[i] = temperature_ambient[i-1]
+
 timestamp_time = pd.to_datetime(timestamp_str) #Convert string to TimeIndex format
 
 ##Plot##
 fig, ax = plt.subplots(1)
 fig.autofmt_xdate()
-#loc = plticker.MultipleLocator(base=0.0002) # this locator puts ticks at regular intervals
+#loc = plticker.MultipleLocator(base=0.005) # this locator puts ticks at regular intervals
 #ax.xaxis.set_major_locator(loc)
 
-plt.plot(timestamp_time,temperature_in,linewidth = 1, label = "Temperature IN")
-plt.plot(timestamp_time,temperature_out,linewidth = 1, label = "Temperature OUT")
-plt.plot(timestamp_time,temperature_ambient,linewidth = 1, label = "Temperature AMBIENT")
+plt.plot(timestamp_time,temperature_in,linewidth = 1, label = "Temperature Internal")
+plt.plot(timestamp_time,temperature_out,linewidth = 1, label = "Temperature Surface")
+plt.plot(timestamp_time,temperature_ambient,linewidth = 1, label = "Temperature Ambient")
 
 xfmt = mdates.DateFormatter('%d-%m-%y %H:%M:%S')
 ax.xaxis.set_major_formatter(xfmt)
